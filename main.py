584aa868845499b2bda9901ebe7cc01333bf9ed6
Shilan Shebli <shilanshebli@yahoo.com>
2023-09-09 23:11:26 +0200

    create the database named movie-collection

--- main.py
+++ main.py
@@ -12,23 +12,30 @@ load_dotenv('/Users/shilzli/Documents/Python-Env/EnvironmentVariables/env.txt')
 API_KEY = os.getenv("TMDB_API")
 
 app = Flask(__name__)
-
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+
+all_movies = []
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-collection.db"
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
+
 
 
 @app.route("/")
